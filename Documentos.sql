ca4ef78a4f7c4f15f3382bce31779f79f9fec0c4
iagogodoyluz-hub <iago.godoy.luz@escola.pr.gov.br>
2026-05-22 17:25:01 -0300

    Add files via upload

---
+++ Documentos.sql
@@ -0,0 +1,39 @@
+CREATE DATABASE	 JSON_DATABASE;
+USE JASON_DATABASE;
+
+CREATE TABLE dados_json(
+json char (250)
+);
+
+INSERT INTO dados_json VALUES(
+'{
+"nome" : "josé", 
+"sobrenome" : "cardoso",
+"idade" : "25",
+"ID": "001"
+)');
+SELECT * FROM dados_json;
+
+CREATE TABLE json_dados(
+teste_json json 
+);
+INSERT INTO json_dados VALUES( 
+'(
+"nome" : "maria"
+"sobrenome" : "silva",
+"idade" : "23",
+"ID" : "002"
+)');
+
+
+
+
+
+,
+
+,
+,,
+,
+
+,
+,323232322332656565
